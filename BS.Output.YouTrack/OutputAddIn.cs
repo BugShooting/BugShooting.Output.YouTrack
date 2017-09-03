c4ab87a5e515070e98a079eb01f291e636333efd
Alexej Hirsch <alexej@bugshooting.com>
2017-09-03 10:59:18 +0200

    Projektdateien hinzufügen.

---
+++ BS.Output.YouTrack/OutputAddIn.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BS.Output.YouTrack
+{
+  public class OutputAddIn: V3.OutputAddIn<Output>
+  {
+
+    protected override string Name
+    {
+      get { return "YouTrack"; }
+    }
+
+    protected override Image Image64
+    {
+      get  { return Properties.Resources.logo_64; }
+    }
+
+    protected override Image Image16
+    {
+      get { return Properties.Resources.logo_16 ; }
+    }
+
+    protected override bool Editable
+    {
+      get { return true; }
+    }
+
+    protected override string Description
+    {
+      get { return "Attach screenshots to YouTrack issues."; }
+    }
+    
+    protected override Output CreateOutput(IWin32Window Owner)
+    {
+      
+      Output output = new Output(Name, 
+                                 String.Empty, 
+                                 String.Empty, 
+                                 String.Empty, 
+                                 "Screenshot",
+                                 String.Empty, 
+                                 true,
+                                 String.Empty,
+                                 String.Empty);
+
+      return EditOutput(Owner, output);
+
+    }
+
+    protected override Output EditOutput(IWin32Window Owner, Output Output)
+    {
+
+      Edit edit = new Edit(Output);
+
+      var ownerHelper = new System.Windows.Interop.WindowInteropHelper(edit);
+      ownerHelper.Owner = Owner.Handle;
+      
+      if (edit.ShowDialog() == true) {
+
+        return new Output(edit.OutputName,
+                          edit.Url,
+                          edit.UserName,
+                          edit.Password,
+                          edit.FileName,
+                          edit.FileFormat,
+                          edit.OpenItemInBrowser,
+                          Output.LastProjectID,
+                          Output.LastIssueID);
+      }
+      else
+      {
+        return null; 
+      }
+
+    }
+
+    protected override OutputValueCollection SerializeOutput(Output Output)
+    {
+
+      OutputValueCollection outputValues = new OutputValueCollection();
+
+      outputValues.Add(new OutputValue("Name", Output.Name));
+      outputValues.Add(new OutputValue("Url", Output.Url));
+      outputValues.Add(new OutputValue("UserName", Output.UserName));
+      outputValues.Add(new OutputValue("Password",Output.Password, true));
+      outputValues.Add(new OutputValue("OpenItemInBrowser", Convert.ToString(Output.OpenItemInBrowser)));
+      outputValues.Add(new OutputValue("FileName", Output.FileName));
+      outputValues.Add(new OutputValue("FileFormat", Output.FileFormat));
+      outputValues.Add(new OutputValue("LastProjectID", Output.LastProjectID));
+      outputValues.Add(new OutputValue("LastIssueID", Output.LastIssueID));
+
+      return outputValues;
+      
+    }
+
+    protected override Output DeserializeOutput(OutputValueCollection OutputValues)
+    {
+
+      return new Output(OutputValues["Name", this.Name].Value,
+                        OutputValues["Url", ""].Value, 
+                        OutputValues["UserName", ""].Value,
+                        OutputValues["Password", ""].Value, 
+                        OutputValues["FileName", "Screenshot"].Value, 
+                        OutputValues["FileFormat", ""].Value,
+                        Convert.ToBoolean(OutputValues["OpenItemInBrowser", Convert.ToString(true)].Value),
+                        OutputValues["LastProjectID", string.Empty].Value, 
+                        OutputValues["LastIssueID", string.Empty].Value);
+
+    }
+
+    protected override async Task<V3.SendResult> Send(IWin32Window Owner, Output Output, V3.ImageData ImageData)
+    {
+
+      try
+      {
+
+        string userName = Output.UserName;
+        string password = Output.Password;
+        bool showLogin = string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(password);
+        bool rememberCredentials = false;
+
+        string fileName = V3.FileHelper.GetFileName(Output.FileName, Output.FileFormat, ImageData);
+
+        while (true)
+        {
+
+          if (showLogin)
+          {
+
+            // Show credentials window
+            Credentials credentials = new Credentials(Output.Url, userName, password, rememberCredentials);
+
+            var credentialsOwnerHelper = new System.Windows.Interop.WindowInteropHelper(credentials);
+            credentialsOwnerHelper.Owner = Owner.Handle;
+
+            if (credentials.ShowDialog() != true)
+            {
+              return new V3.SendResult(V3.Result.Canceled);
+            }
+
+            userName = credentials.UserName;
+            password = credentials.Password;
+            rememberCredentials = credentials.Remember;
+
+          }
+
+          LoginResult loginResult = await YouTrackProxy.Login(Output.Url, userName, password);
+          if (!loginResult.Success)
+          {
+            showLogin = true;
+            continue;
+          }
+
+          // Get projects
+          List<Project> projects = await YouTrackProxy.GetAccessibleProjects(Output.Url, loginResult.LoginCookies);
+
+          // Show send window
+          Send send = new Send(Output.Url, Output.LastProjectID, Output.LastIssueID, projects, userName, password, fileName);
+
+          var sendOwnerHelper = new System.Windows.Interop.WindowInteropHelper(send);
+          sendOwnerHelper.Owner = Owner.Handle;
+
+          if (!send.ShowDialog() == true)
+          {
+            return new V3.SendResult(V3.Result.Canceled);
+          }
+
+          string projectID = null;
+          string issueID = null;
+
+          if (send.CreateNewIssue)
+          {
+
+            projectID = send.ProjectID;
+
+            CreateIssueResult createIssueResult = await YouTrackProxy.CreateIssue(Output.Url, loginResult.LoginCookies, projectID, send.Summary, send.Description);
+            if (!createIssueResult.Success)
+            {
+              return new V3.SendResult(V3.Result.Failed, createIssueResult.FaultMessage);
+            }
+
+            issueID = createIssueResult.IssueID;
+
+          }
+          else
+          {
+            issueID = send.IssueID;
+            projectID = Output.LastProjectID;
+          }
+
+          string fullFileName = String.Format("{0}.{1}", send.FileName, V3.FileHelper.GetFileExtention(Output.FileFormat));
+          string fileMimeType = V3.FileHelper.GetMimeType(Output.FileFormat);
+          byte[] fileBytes = V3.FileHelper.GetFileBytes(Output.FileFormat, ImageData);
+
+          AddAttachmentResult addAttachmentResult = await YouTrackProxy.AddAttachment(Output.Url, loginResult.LoginCookies, issueID, fullFileName, fileBytes, fileMimeType);
+          if (!addAttachmentResult.Success)
+          {
+            return new V3.SendResult(V3.Result.Failed, addAttachmentResult.FaultMessage);
+          }
+
+          // Open issue in browser
+          if (Output.OpenItemInBrowser)
+          {
+            V3.WebHelper.OpenUrl(String.Format("{0}/issue/{1}", Output.Url, issueID));
+          }
+
+
+          return new V3.SendResult(V3.Result.Success,
+                                    new Output(Output.Name,
+                                               Output.Url,
+                                               (rememberCredentials) ? userName : Output.UserName,
+                                               (rememberCredentials) ? password : Output.Password,
+                                               Output.FileName,
+                                               Output.FileFormat,
+                                               Output.OpenItemInBrowser,
+                                               projectID,
+                                               issueID));
+            
+        }
+
+      }
+      catch (Exception ex)
+      {
+        return new V3.SendResult(V3.Result.Failed, ex.Message);
+      }
+
+    }
+
+  }
+}
---
+++ BS.Output.YouTrack/YouTrackProxy.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml;
+
+namespace BS.Output.YouTrack
+{
+  internal class YouTrackProxy
+  {
+
+    static internal async Task<LoginResult> Login(string url, string userName, string password)
+    {
+      
+      try
+      {
+
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(String.Format("{0}/rest/user/login", url));
+        request.CookieContainer = new CookieContainer();
+        request.ContentType = "application/x-www-form-urlencoded";
+        request.KeepAlive = true;
+        request.Method = "POST";
+        
+        StringBuilder postData = new StringBuilder();
+        postData.AppendFormat("login={0}&password={1}",userName, password);
+        
+        byte[] postBytes = Encoding.UTF8.GetBytes(postData.ToString());
+        using (Stream requestStream = await request.GetRequestStreamAsync())
+        {
+          await requestStream.WriteAsync(postBytes, 0, postBytes.Length);
+        }
+
+        using (HttpWebResponse response = (HttpWebResponse) await request.GetResponseAsync())
+        { 
+          using (Stream responseStream = response.GetResponseStream())
+          {
+            using (StreamReader reader = new StreamReader(responseStream))
+            {
+              string responseData = reader.ReadToEnd();
+
+              if (responseData.ToLower() == "<login>ok</login>")
+              {
+                return new LoginResult(true, response.Cookies);
+              }
+              else
+              {
+                return new LoginResult(true, null);
+              }
+
+            }
+          }
+        }
+
+      }
+      catch
+      {
+        return new LoginResult(false, null);
+      }
+    }
+
+    static internal async Task<List<Project>> GetAccessibleProjects(string url, CookieCollection loginCookies)
+    {
+
+      HttpWebRequest request = (HttpWebRequest)WebRequest.Create(String.Format("{0}/rest/project/all", url));
+      request.CookieContainer = new CookieContainer();
+      request.KeepAlive = true;
+      request.Method = "GET";
+
+      request.CookieContainer = new CookieContainer();
+      if ((loginCookies != null))
+      {
+        request.CookieContainer.Add(loginCookies);
+      }
+
+      string responseData = null;
+      using (HttpWebResponse response = (HttpWebResponse) await request.GetResponseAsync())
+      { 
+        using (Stream responseStream = response.GetResponseStream())
+        {
+          using (StreamReader reader = new StreamReader(responseStream))
+          {
+            responseData = reader.ReadToEnd();
+          }
+        }
+      }
+      
+      XmlDocument xmlDoc = new XmlDocument();
+      xmlDoc.LoadXml(responseData);
+
+      List<Project> projects = new List<Project>();
+
+      foreach (XmlNode node in xmlDoc.GetElementsByTagName("project"))
+      {
+        projects.Add(new Project(node.Attributes["shortName"].Value, node.Attributes["name"].Value));
+      }
+
+      return projects;
+
+    }
+
+    static internal async Task<CreateIssueResult> CreateIssue(string url, CookieCollection loginCookies, string projectID, string summary, string description)
+    {
+
+      try
+      {
+
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(String.Format("{0}/rest/issue?project={1}&summary={2}&description={3}", url, HttpUtility.UrlEncode(projectID), HttpUtility.UrlEncode(summary), HttpUtility.UrlEncode(description)));
+        request.CookieContainer = new CookieContainer();
+        request.Method = "PUT";
+        
+        request.CookieContainer = new CookieContainer();
+        if ((loginCookies != null))
+        {
+          request.CookieContainer.Add(loginCookies);
+        }
+
+        // Add fake content, necessary for the YouTrack API
+        byte[] contentBytes = Encoding.UTF8.GetBytes("--");
+        request.ContentLength = contentBytes.Length;
+        using (Stream requestStream = await request.GetRequestStreamAsync())
+        {
+          await requestStream.WriteAsync(contentBytes, 0, contentBytes.Length);
+        }
+
+        using (HttpWebResponse response = (HttpWebResponse) await request.GetResponseAsync())
+        {
+          string[] values = response.Headers["Location"].Split('/');
+          return new CreateIssueResult(true, values[values.Length - 1], null);
+        }
+
+      }
+      catch (WebException ex)
+      {
+        HttpWebResponse response = (HttpWebResponse)ex.Response;
+        return new CreateIssueResult(false, null, response.StatusDescription);
+      }
+    }
+
+    static internal async Task<AddAttachmentResult> AddAttachment(string url, CookieCollection loginCookies, string issueID, string fullFileName, byte[] fileBytes, string fileMimeType)
+    {
+
+      try
+      {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(String.Format("{0}/rest/issue/{1}/attachment", url, issueID));
+
+        string boundary = String.Format("----------{0}", DateTime.Now.Ticks.ToString("x"));
+
+        request.CookieContainer = new CookieContainer();
+        request.KeepAlive = true;
+        request.ContentType = "multipart/form-data; boundary=" + boundary;
+        request.Method = "POST";
+
+        request.CookieContainer = new CookieContainer();
+        if ((loginCookies != null))
+        {
+          request.CookieContainer.Add(loginCookies);
+        }
+        
+        StringBuilder postData = new StringBuilder();
+        postData.AppendFormat("--{0}", boundary);
+        postData.AppendLine();
+        postData.AppendFormat("Content-Disposition: form-data; name=\"{0}\"; filename=\"{0}\"\r\n", fullFileName);
+        postData.AppendFormat("Content-Type: {0}\r\n", fileMimeType);
+        postData.AppendFormat("Content-Transfer-Encoding: binary\r\n");
+        postData.AppendLine();
+
+        byte[] postBytes = Encoding.UTF8.GetBytes(postData.ToString());
+        byte[] boundaryBytes = Encoding.ASCII.GetBytes(String.Format("\r\n--{0}--\r\n", boundary));
+
+        request.ContentLength = postBytes.Length + fileBytes.Length + boundaryBytes.Length;
+
+        using (Stream requestStream = await request.GetRequestStreamAsync())
+        {
+          requestStream.Write(postBytes, 0, postBytes.Length);
+          requestStream.Write(fileBytes, 0, fileBytes.Length);
+          requestStream.Write(boundaryBytes, 0, boundaryBytes.Length);
+        }
+        
+        using (HttpWebResponse response = (HttpWebResponse) await request.GetResponseAsync())
+        {
+          return new AddAttachmentResult(true, null);
+        }
+        
+      }
+      catch (WebException ex)
+      {
+        HttpWebResponse response = (HttpWebResponse)ex.Response;
+        return new AddAttachmentResult(false, response.StatusDescription);
+      }
+    }
+
+  }
+
+  internal class LoginResult
+  {
+
+    bool success;
+    CookieCollection loginCookies;
+
+    public LoginResult(bool success,
+                       CookieCollection loginCookies)
+    {
+      this.success = success;
+      this.loginCookies = loginCookies;
+    }
+
+    public bool Success
+    {
+      get { return success; }
+    }
+
+    public CookieCollection LoginCookies
+    {
+      get { return loginCookies; }
+    }
+
+  }
+
+  internal class CreateIssueResult
+  {
+
+    bool success;
+    string issueID;
+    string faultMessage;
+
+    public CreateIssueResult(bool success,
+                             string issueID,
+                             string faultMessage)
+    {
+      this.success = success;
+      this.issueID = issueID;
+      this.faultMessage = faultMessage;
+    }
+
+    public bool Success
+    {
+      get { return success; }
+    }
+
+    public string IssueID
+    {
+      get { return issueID; }
+    }
+
+    public string FaultMessage
+    {
+      get { return faultMessage; }
+    }
+
+  }
+
+  internal class AddAttachmentResult
+  {
+
+    bool success;
+    string faultMessage;
+
+    public AddAttachmentResult(bool success,
+                               string faultMessage)
+    {
+      this.success = success;
+      this.faultMessage = faultMessage;
+    }
+
+    public bool Success
+    {
+      get { return success; }
+    }
+
+    public string FaultMessage
+    {
+      get { return faultMessage; }
+    }
+
+  }
+
+  internal class Project
+  {
+
+    private string id;
+    private string name;
+
+    public Project(string id, string name)
+    {
+      this.id = id;
+      this.name = name;
+    }
+
+    public string ID
+    {
+      get { return id; }
+    }
+
+    public string Name
+    {
+      get { return name; }
+    }
+
+  }
+
+}
